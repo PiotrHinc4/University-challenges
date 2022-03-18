ce0a9393d93eefde02bfa49eb8e38f0a85f5a83f
Piotr Hinc <piotr.hinc4@gmail.com>
2022-03-18 19:19:01 +0100

    Add solution for SPOJ_1

---
+++ University challenges/SPOJ_1/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number = 0;
+	while (cin >> number)
+	{
+		if (number == 42)
+		{
+			break;
+		}
+		cout << number << "\n";
+	}
+	return 0;
+}
