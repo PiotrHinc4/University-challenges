acbe9917c3d137f4dac9d252c50c39ff7bdbf6c2
Piotr Hinc <piotr.hinc4@gmail.com>
2022-04-13 11:20:51 +0200

    Add second version to solution for SPOJ_2

---
+++ University challenges/SPOJ_2_version_2/Source.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void prime_numbers(int first_number, int last_number)
+{
+	int range_size = last_number - first_number;
+	int range[11];
+	int actual_number = 2;
+
+
+	for (int indeks = 0; indeks <= last_number; indeks++)
+	{
+		range[indeks] = indeks;
+	}
+
+	for (int indeks = 2; indeks <= sqrt(last_number); indeks++)
+	{
+		for (int indeks2 = indeks; indeks2 <= last_number; indeks2++)
+		{
+			if (indeks2 != actual_number && indeks2 % actual_number == 0)
+			{
+				range[indeks2] = 0;
+			}
+		}
+		actual_number++;
+	}
+
+	for (int indeks = 0; indeks <= last_number; indeks++)
+	{
+		if (range[indeks]>1&&range[indeks]>=first_number)
+		{
+			cout << range[indeks] << "\n";
+		}
+	}
+}
+
+int main()
+{
+	unsigned short how_many_times_to_use = 0;
+	unsigned int first_number = 0;
+	unsigned int last_number = 0;
+
+	cout << "Write how many times to look for prime numbers in the range: ";
+	cin >> how_many_times_to_use;
+	for (how_many_times_to_use; how_many_times_to_use>0; how_many_times_to_use--)
+	{
+		cout << "Enter numbers in the range: ";
+		cin >> first_number >> last_number;
+		prime_numbers(first_number, last_number);
+	}
+
+	return 0;
+}
